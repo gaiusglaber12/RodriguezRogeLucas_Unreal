215814d0b45906c1580b0288229f492e9b82c573
Lucas Rodriguez Roge <lucasrodriguezroge@pathfindersstudio.com>
2022-08-15 11:09:53 -0300

    feat(common): init proyect

---
+++ Source/UnrealImage/UnrealImageGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealImageGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALIMAGE_API AUnrealImageGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
